603f08fc7441f0fd3bb0a88b3dc21c6c08c4778f
devgroot22 <devgroot22@gmail.com>
2023-08-02 16:12:28 +0900

    Only pull request and workfloW_dispatch

---
+++ main.py
@@ -0,0 +1,70 @@
+
+
+
+
+
+
+
+class Person:
+
+
+
+
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def __del__(self):
+        print(f'Object is being deconstructed!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+p = Person("Mike", 25)
+del p
+
